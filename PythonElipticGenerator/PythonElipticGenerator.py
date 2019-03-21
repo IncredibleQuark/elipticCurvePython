008dc8ae3d0e300470fa0fab47351961294439b1
lukasz kupinski <lkupinski@gmail.com>
2019-03-21 01:25:40 +0100

    is prime function

--- PythonElipticGenerator/PythonElipticGenerator.py
+++ PythonElipticGenerator/PythonElipticGenerator.py
@@ -1,1 +1,23 @@
+import math
 
+def is_prime(n):
+
+    if n == 1:
+        return False
+
+    if n == 2:
+      return True
+
+    if n > 2 and n % 2 == 0:
+        return False
+
+    max_divisor = math.floor(math.sqrt(n))
+
+    for d in range(3, 1 + max_divisor, 2):
+        if n % d == 0:
+            return False
+    return True
+
+
+for n in range(1, 21):
+    print(n, is_prime(n))
